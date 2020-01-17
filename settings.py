edbcffa91a9e6c44d99a342f2b8473ab7a62872c
You-Yi Jau <yyjau@ubuntu.pxeserver>
2020-01-17 07:21:45 -0800

    edit settings back to default

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ paths defined here are used in many places
 DATA_PATH = 'datasets' # path for datasets
 EXPER_PATH = 'logs' # path for saving checkpoints
 SYN_TMPDIR = './datasets/'  # path for dumping synthetic data
-DEBUG = True # true: will make synthetic data only uses draw_checkboard and ignore other classes
+DEBUG = False # true: will make synthetic data only uses draw_checkboard and ignore other classes
 # DEBUG = False
 
