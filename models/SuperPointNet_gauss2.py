866c40aa4b10bb570dd0ca9b4bda561f1f34cb48
Joshua Kang <guppykang@gmail.com>
2020-07-31 18:28:34 -0700

    returns nothing

--- models/SuperPointNet_gauss2.py
+++ models/SuperPointNet_gauss2.py
@@ -106,6 +106,8 @@ def get_matches(deses_SP):
     # tracker = PointTracker(max_length=2, nn_thresh=1.2)
     # print("deses_SP[1]: ", deses_SP[1].shape)
     matching_mask = tracker.nn_match_two_way(f(deses_SP[0]), f(deses_SP[1]), nn_thresh=1.2)
+    return matching_mask
+
     # print("matching_mask: ", matching_mask.shape)
     # f_mask = lambda pts, maks: pts[]
     # pts_m = []
